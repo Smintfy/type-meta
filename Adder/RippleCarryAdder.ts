8f465dfb667a5b7c93d920b6f655cd0bd829cc78
sveltie <lucydabestt@gmail.com>
2024-08-21 17:29:35 +0800

    Simplify RCA

--- Adder/RippleCarryAdder.ts
+++ Adder/RippleCarryAdder.ts
@@ -19,17 +19,14 @@ type RippleCarryAdder<
                 ? RippleCarryAdder<RestA, RestB, Cout, [S, ...Sum]>
                 : never
             : never
-        : B extends [infer Y extends Bit, ...infer RestB extends Array<Bit>]
-            ? never
-            // Insert the final carry to the final sum if there's one
-            : Cin extends 1
-                ? [1, ...Sum]
-                : Sum
+        : Cin extends 1
+            ? [1, ...Sum]
+            : Sum
 
 // Why reverse?
 // Because our Ripple Carry Adder works by adding the left most bit first.
 // That's why we need to reverse our input so the right most bit becomes,
 // the left most bit.
-type A = Reverse<[1, 1, 1, 1]> // 15
+type A = Reverse<[1, 1, 0, 1]> // 13
 type B = Reverse<[0, 1, 1, 1]> // 7
-type Result = RippleCarryAdder<A, B> // Resolves to 19
+type Result = RippleCarryAdder<A, B> // Resolves to 20
