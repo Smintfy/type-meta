bd9f815c018ef14cea5bb9c3b3fc91a469e5d8aa
sveltie <lucydabestt@gmail.com>
2024-08-21 17:26:16 +0800

    Refactor the RCA and Full Adder

---
+++ Adder/RippleCarryAdder.ts
@@ -0,0 +1,35 @@
+import { Bit } from "./Bit"
+import { FullAdder } from "./FullAdder"
+
+type Reverse<T extends any[]> = T extends [infer First, ...infer Rest]
+    ? [...Reverse<Rest>, First]
+    : [];
+
+type RippleCarryAdder<
+    A extends Array<Bit>,
+    B extends Array<Bit>,
+    Cin extends Bit = 0,
+    Sum extends Array<Bit> = []
+> = A extends [infer X extends Bit, ...infer RestA extends Array<Bit>]
+        ? B extends [infer Y extends Bit, ...infer RestB extends Array<Bit>]
+            // Add the first element of A and B with the full adder
+            // Extract the carry out and sum from the full adder
+            ? FullAdder<X, Y, Cin> extends [infer Cout extends Bit, infer S extends Bit]
+                // Add the remaining bits
+                ? RippleCarryAdder<RestA, RestB, Cout, [S, ...Sum]>
+                : never
+            : never
+        : B extends [infer Y extends Bit, ...infer RestB extends Array<Bit>]
+            ? never
+            // Insert the final carry to the final sum if there's one
+            : Cin extends 1
+                ? [1, ...Sum]
+                : Sum
+
+// Why reverse?
+// Because our Ripple Carry Adder works by adding the left most bit first.
+// That's why we need to reverse our input so the right most bit becomes,
+// the left most bit.
+type A = Reverse<[1, 1, 1, 1]> // 15
+type B = Reverse<[0, 1, 1, 1]> // 7
+type Result = RippleCarryAdder<A, B> // Resolves to 19
